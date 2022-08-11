4829afad1b00f1cd1531d89c36d83e8259bdfee8
loki <loki@wondermove.net>
2022-08-12 01:13:05 +0900

    2022.08.12. 01:13

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,10 @@
 
 use tauri::Manager;
 use tauri::SystemTray;
-use tauri_plugin_autostart::MacosLauncher;
 
 fn main() {
   let system_tray = SystemTray::new();
   tauri::Builder::default()
-    .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, true))
     .system_tray(system_tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
