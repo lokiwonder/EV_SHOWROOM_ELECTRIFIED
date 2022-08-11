11f17fa48d4ff0919b3d7821656cf0fb5d28fec9
loki <loki@wondermove.net>
2022-08-11 10:54:23 +0900

    2022.08.11. 10:54

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,14 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
 use tauri::Manager;
 use tauri_plugin_autostart::MacosLauncher;
 
 fn main() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, false /* hidden flag */))
+    .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, false))
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
