da752b8234a29a3caa5b7267a3df00661657b15f
loki <loki@wondermove.net>
2022-08-12 10:45:33 +0900

    2022.08.12 10:45

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,7 +20,7 @@ fn auto_start() {
   // description: Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run 레지스트리 불러오기 \\
   let regkey = Hive::CurrentUser.open(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", Security::AllAccess).unwrap();
   // description: 불러온 레지스트리에 value 저장 //
-  regkey.set_value("EV CMS", &Data::String(r"C:Users\lacls\Documents\Programing\ev_cms\src-tauri\target\debug\EV SHOWROOM Electrified.exe".try_into().unwrap())).unwrap();
+  regkey.set_value("EV CMS", &Data::String(r"C:\Users\lacls\Documents\Programing\ev_cms\src-tauri\target\debug\EV SHOWROOM Electrified.exe".try_into().unwrap())).unwrap();
 }
 
 fn main() {
